7e1fad5382988b081a4fe08aa927d8f3badb5365
Kemal <88620080+Kemal04@users.noreply.github.com>
2024-03-29 00:01:58 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/home/",
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
+  base: "/",
 })
